f6fc4274e80e0c6733f33035eda792eb64d624a3
Safwan Bin Shahjehan <safwanshah.work@gmail.com>
2018-07-12 23:05:22 +0800

    First commit!

---
+++ Camera access/Camera access/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+
+namespace Camera_access
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        public Form1()
+        {
+            InitializeComponent();
+            Run();
+
+        }
+
+        private void Run()
+        {
+            try
+            {
+                capture = new VideoCapture();
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return;
+            }
+            Application.Idle += ProcessFrame;     //For the application to process the image frames
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            imageBox1.Image = capture.QueryFrame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
